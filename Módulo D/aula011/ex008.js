fe32de73544f8fbf91812d489a89bd5d724d7659
PedroMesquitaa <pedrozd45@gmail.com>
2023-01-24 00:21:44 -0300

    Novos exercicios de JS "Condições"

---
+++ Módulo D/aula011/ex008.js
@@ -0,0 +1,13 @@
+//console.log('O console funcionou corretamente!')
+
+var vel = 280
+
+console.log(`A velocidade do seu carro é: ${vel}Km/h`)
+
+if(vel > 60){
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}else{
+    console.log(`Você está dentro do limite de velocidade`)
+}
+
+console.log(`Diriga sempre usando cinto de segurança!`)
