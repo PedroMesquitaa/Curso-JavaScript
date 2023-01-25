485836fcd73c0e168d77b57c3be1a50d625bb50b
PedroMesquitaa <pedrozd45@gmail.com>
2023-01-24 23:04:26 -0300

    Desafio da idade

---
+++ Desafios/desafio-da-idade/script.js
@@ -0,0 +1,58 @@
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+
+    var fano= document.getElementById('txtano')
+    var res = document.getElementById('res')
+
+    if(fano.value.length == 0 || Number(fano.value) > ano){
+        alert('[ERRO] Verifique os dados e tente novamente!')
+    }else{
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        if(fsex[0].checked){
+            genero = 'Homem'
+            if(idade >= 0 && idade <= 2){
+                // Bebê
+                img.setAttribute('src', 'imagens/bebe-M.jpg')
+            }else if(idade >= 3 && idade <= 11){
+                // Criança
+                img.setAttribute('src', 'imagens/garoto-M.jpg')
+            }else if(idade >= 12 && idade <= 21){
+                // Jovem
+                img.setAttribute('src', 'imagens/Jovem-M.jpg')
+            }else if(idade >= 22 && idade <= 70){
+                // Adulto
+                img.setAttribute('src', 'imagens/adulto-M.jpg')
+            }else{
+                // Idoso
+                img.setAttribute('src', 'imagens/Idoso-M.jpg')
+            }
+        }else if (fsex[1].checked){
+            genero = 'Mulher'
+            if(idade >= 0 && idade <= 2){
+                // Bebê
+                img.setAttribute('src', 'imagens/bebe-F.jpg')
+            }else if(idade >= 3 && idade <= 11){
+                // Criança
+                img.setAttribute('src', 'imagens/garota-F.jpg')
+            }else if(idade >= 12 && idade <= 21){
+                // Jovem
+                img.setAttribute('src', 'imagens/jovem-F.jpg')
+            }else if(idade >= 22 && idade <= 70){
+                // Adulto
+                img.setAttribute('src', 'imagens/adulta-F.jpg')
+            }else{
+                // Idoso
+                img.setAttribute('src', 'imagens/idosa-F.jpg')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `<p>Detectamos ${genero} com ${idade} anos.</p>`
+        res.appendChild(img)
+    }
+}
