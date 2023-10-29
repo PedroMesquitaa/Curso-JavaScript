9b0790b82c2ee45de7c9891e9e136bfd05eb9ac3
PedroMesquitaa <pedrozd45@gmail.com>
2023-10-29 13:48:40 -0300

    Melhorando o código
    Diminuindo o tamanho do código separando por funções

--- Curso 2/introducao-javascript/js/calcula-imc.js
+++ Curso 2/introducao-javascript/js/calcula-imc.js
@@ -1,27 +1,32 @@
-var titulo = document.querySelector("h1");
-titulo.textContent = "Aparecida Nutricionista";
+var titulo = document.querySelector("h1");  // querySelector retorna no console para nós o h1
+titulo.textContent = "Aparecida Nutricionista"; // Usando o textContext muda o que esta dentro.
 
-var pacientes = document.querySelectorAll(".paciente");
+var pacientes = document.querySelectorAll(".paciente"); // querySelectorAll retorna todos o conteudo que tem a mesma classe
 
-for(var i = 0; i < pacientes.length; i++){
+// fazendo o calculo para todos os pacientes que tem no formulário sem precisar fazer um código para cada um
 
-	var paciente = pacientes[i];
+for(var i = 0; i < pacientes.length; i++){   // variavel i igual a 0; enquanto o total(length) for menos que i; adiciona(++) na varial 1, ou seja, percorre o loop mais uma vez. 
 
-	var tdPeso = paciente.querySelector(".info-peso");
-	var peso = tdPeso.textContent;
+	var paciente = pacientes[i];  // a variavel paciente vai receber o conteudo de pacientes a cada loop.
 
-	var tdAltura = paciente.querySelector(".info-altura");
-	var altura = tdAltura.textContent
+	var tdPeso = paciente.querySelector(".info-peso"); // varialvel tdPeso vai receber a classe info-peso que esta dentro do paciente.
+	var peso = tdPeso.textContent; // variavel peso ira receber o conteudo do tdPeso
 
-	var tdImc = paciente.querySelector(".info-imc");
+	var tdAltura = paciente.querySelector(".info-altura"); // varialvel tdAltura vai receber a classe info-altura que esta dentro do paciente.
+	var altura = tdAltura.textContent // variavel altura ira receber o conteudo do tdaltura
 
-	var pesoValido = true;
-	var alturaValida = true;
+
+	var tdImc = paciente.querySelector(".info-imc"); // varialvel tdImc vai receber a classe info-imc que esta dentro do paciente.
+
+	var pesoValido = true; // valor da variavel pesoValido inicial é verdadeiro e usaremos isso para validação
+	var alturaValida = true; // valor da variavel alturaValida inicial é verdadeiro e usaremos isso para validação
+
+	// fazendo validação
 
 	if(peso <= 0 || peso >= 400){
-		tdImc.textContent = "Peso inválido!";
-		pesoValido = false;
-		paciente.classList.add("paciente-invalido");
+		tdImc.textContent = "Peso inválido!";  // Se o peso for invalido a var tdImc ira mostrar um conteudo de texto
+		pesoValido = false;  // o valor do pesoValido sera falso
+		paciente.classList.add("paciente-invalido"); // sendo falso o valor ele ira usar classList.add do css para usar o estilo de la
 	}
 
 	if(altura <= 0 || altura >= 2.40){
@@ -31,7 +36,17 @@ for(var i = 0; i < pacientes.length; i++){
 	}
 
 	if(alturaValida == true && pesoValido == true){
-		var imc = peso / (altura * altura);
-		tdImc.textContent = imc.toFixed(2);
+		var imc = calculaImc(peso,altura); // a var imc ira receber a funcao calculaImc
+		tdImc.textContent = imc; // e a var tdImc ira nos mostrar como um conteudo de texto o valor do calculo
 	}
+}
+
+// funcao para fazer o calculo onde podera ser reutilizado em outro lugar, como por exemplo para fazer o calculo direto quando for adicionado paciente novo
+
+function calculaImc(peso,altura){  // funcao calculaImc tera os valores puxados das var peso e altura
+	var imc = 0;
+
+	imc = peso / (altura * altura);
+
+	return imc.toFixed(2);	// ira retornar o valor do imc usando .toFixed(2) para só ter 2 casa decimais
 }
--- Curso 2/introducao-javascript/js/form.js
+++ Curso 2/introducao-javascript/js/form.js
@@ -1,35 +1,66 @@
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 
+// criando dentro da var botaoAdicionar um evento de escuta que sera o click do mouse e fazendo uma funcao anonima
+
+// e que quando clicado no botao adicionar ira fazer essa funcao de criacao de formulario
+
 botaoAdicionar.addEventListener("click", function(event){
-	event.preventDefault();
+	event.preventDefault(); // serve para o padrao do evento nao ser usado.
+
+	var form = document.querySelector("#form-adiciona");  
+
+    // Extraindo informacoes do paciente do form
+    var paciente = obtemPacienteDoFormulario(form);
 	
-	var form = document.querySelector("#form-adiciona");
-
-	var nome = form.nome.value;
-	var peso = form.peso.value;
-	var altura = form.altura.value;
-	var gordura = form.gordura.value;
 	
-	var pacienteTr = document.createElement("tr");
+    //sera criado um elemento no html tr e td
+	var pacienteTr = montaTr(paciente);
 
-	var nomeTd = document.createElement("td");
-	var pesoTd = document.createElement("td");
-	var alturaTd = document.createElement("td");
-	var gorduraTd = document.createElement("td");
-	var imcTd = document.createElement("td");
+    // adicionando o paciente na tabela
+	var tabela = document.querySelector("#tabela-pacientes");  // a var tabela ira pegar o conteudo do id tabela-pacientes
 
-	nomeTd.textContent = nome;
-	pesoTd.textContent = peso;
-	alturaTd.textContent = altura;
-	gorduraTd.textContent = gordura;
+	tabela.appendChild(pacienteTr);  // cria o pacienTr dentro tabela
 
-	pacienteTr.appendChild(nomeTd);
-	pacienteTr.appendChild(pesoTd);
-	pacienteTr.appendChild(alturaTd);
-	pacienteTr.appendChild(gorduraTd);
-
-	var tabela = document.querySelector("#tabela-pacientes");
-
-	tabela.appendChild(pacienteTr);
+    form.reset();
 
 });
+
+
+// Extraindo informacoes do paciente do form
+function obtemPacienteDoFormulario(form){
+    // criando um OBJETO paciente
+    var paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,      // o .value ele extrai informacoes do paciente do form
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        imc: calculaImc(form.peso.value, form.altura.value)
+    }
+
+    return paciente;
+}
+
+
+//sera criado um elemento no html tr e td
+function montaTr(paciente){
+    var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
+	
+	pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+	pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));     // o appendChild serve para ensinar a maquina a saber qual var ira ser o pai de outra var, ou seja, a tag tr é pai da td pois a td fica dentro da tr
+	pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
+	pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
+    pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
+
+
+    return pacienteTr;
+}
+
+//sera criado td com a class
+function montaTd(dado,classe){
+    var td = document.createElement("td");
+    td.textContent = dado;
+    td.classList.add(classe);
+
+    return td;
+}
