6e98bbb3b689d72bcd0550eefe7ad805e11e7656
PedroMesquitaa <pedrozd45@gmail.com>
2023-10-28 21:17:50 -0300

    Aprendendo a usar Eventos, formularios e criando elementos.
    Uso de appendChild, createElement, .value e uso de function

--- Curso 2/introducao-javascript/js/principal.js
+++ Curso 2/introducao-javascript/js/principal.js
@@ -21,17 +21,54 @@ for(var i = 0; i < pacientes.length; i++){
 	if(peso <= 0 || peso >= 400){
 		tdImc.textContent = "Peso inválido!";
 		pesoValido = false;
+		paciente.classList.add("paciente-invalido");
 	}
 
 	if(altura <= 0 || altura >= 2.40){
 		tdImc.textContent = "Altura inválida";
 		alturaValida = false;
+		paciente.classList.add("paciente-invalido");
 	}
 
 	if(alturaValida == true && pesoValido == true){
 		var imc = peso / (altura * altura);
 		tdImc.textContent = imc.toFixed(2);
 	}
-
-
 }
+
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+
+botaoAdicionar.addEventListener("click", function(event){
+	event.preventDefault();
+	
+	var form = document.querySelector("#form-adiciona");
+
+	var nome = form.nome.value;
+	var peso = form.peso.value;
+	var altura = form.altura.value;
+	var gordura = form.gordura.value;
+	
+	var pacienteTr = document.createElement("tr");
+
+	var nomeTd = document.createElement("td");
+	var pesoTd = document.createElement("td");
+	var alturaTd = document.createElement("td");
+	var gorduraTd = document.createElement("td");
+	var imcTd = document.createElement("td");
+
+	nomeTd.textContent = nome;
+	pesoTd.textContent = peso;
+	alturaTd.textContent = altura;
+	gorduraTd.textContent = gordura;
+
+	pacienteTr.appendChild(nomeTd);
+	pacienteTr.appendChild(pesoTd);
+	pacienteTr.appendChild(alturaTd);
+	pacienteTr.appendChild(gorduraTd);
+
+	var tabela = document.querySelector("#tabela-pacientes");
+
+	tabela.appendChild(pacienteTr);
+
+});
