bfad05d82660c69714052b7f6328300b51daade5
PedroMesquitaa <pedrozd45@gmail.com>
2024-10-07 15:17:26 -0300

    Comportamentos de classes

---
+++ Curso 4/index.js
@@ -0,0 +1,41 @@
+// Todo cliente ira conter o que esta dentre do classe
+class Cliente {
+    nome;
+    cpf;
+}
+
+class ContaCorrente{
+    agencia;
+    saldo;
+
+    sacar(valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+        }
+    }
+}
+
+const cliente1 = new Cliente();
+
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11122233309;
+
+const cliente2 = new Cliente();
+cliente2.nome = 'Alice';
+cliente2.cpf = 88822233307;
+
+
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.saldo = 0;
+contaCorrenteRicardo.agencia = 1001;
+
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.saldo = 100;
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.sacar(200);
+
+console.log(contaCorrenteRicardo.saldo);
+
+
+console.log(cliente1);
+console.log(cliente2);
